a073b32e14ccca9f5aed9684931041f38a9891c0
kyo <phongbinh2511@gmail.com>
2020-03-17 21:34:56 +0800

    [homework-1] Updated implementations of and added unit tests for `DirectedAngleHelper.getDirectedAngle()`.

--- SortingShapes/src/main/java/org/ntutssl/shapes/DirectedAngleHelper.java
+++ SortingShapes/src/main/java/org/ntutssl/shapes/DirectedAngleHelper.java
@@ -2,6 +2,8 @@ package org.ntutssl.shapes;
 
 public final class DirectedAngleHelper
 {
+    private static String ERROR_STARTING_VECTOR_IS_NULL = "The given starting vector is null";
+    private static String ERROR_ENDING_VECTOR_IS_NULL = "The given ending vector is null";
     private DirectedAngleHelper()
     {
         /* Body intentionally empty */
@@ -9,6 +11,14 @@ public final class DirectedAngleHelper
 
     public static double getDirectedAngle( TwoDimensionalVector startingVector, TwoDimensionalVector endingVector )
     {
+        if ( startingVector == null )
+        {
+            throw new NullPointerException( ERROR_STARTING_VECTOR_IS_NULL );
+        }
+        if ( endingVector == null )
+        {
+            throw new NullPointerException( ERROR_ENDING_VECTOR_IS_NULL );
+        }
         return Math.atan2( endingVector.getY(), endingVector.getX() ) - Math.atan2( startingVector.getY(), startingVector.getX() );
     }
 }
